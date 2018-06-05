0e89c7959d5a9ddc13e8414b424fcd8530394ae2
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2018-06-05 19:06:05 +0200

    Add empty Xcode project

---
+++ FinTask/FinTask/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  FinTask
+//
+//  Created by Tomasz Pieczykolan on 05/06/2018.
+//  Copyright © 2018 Tomasz Pieczykolan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    
+    // MARK: - App lifecycle
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
