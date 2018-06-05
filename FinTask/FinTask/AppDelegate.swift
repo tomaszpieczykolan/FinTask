b3671f08caebf12d6216e1ca5cf5612cabb1787f
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2018-06-05 19:14:36 +0200

    Add empty MainViewController

--- FinTask/FinTask/AppDelegate.swift
+++ FinTask/FinTask/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
---
+++ FinTask/FinTask/UI/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  FinTask
+//
+//  Created by Tomasz Pieczykolan on 05/06/2018.
+//  Copyright © 2018 Tomasz Pieczykolan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
