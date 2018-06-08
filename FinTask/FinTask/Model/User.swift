29cdfdeba9411ae4ee01180ab1c1c1e3f4cf5d72
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2018-06-08 19:05:33 +0200

    Add User model

---
+++ FinTask/FinTask/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  FinTask
+//
+//  Created by Tomasz Pieczykolan on 08/06/2018.
+//  Copyright © 2018 Tomasz Pieczykolan. All rights reserved.
+//
+
+import struct Foundation.URL
+
+struct User {
+    let name: String
+    let avatarURL: URL
+    let source: String
+}
