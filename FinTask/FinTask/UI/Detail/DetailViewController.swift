4a888f9f21077e0a602dad756f5a2c8a1a3a157e
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2018-06-10 15:46:35 +0200

    Add empty detail view

---
+++ FinTask/FinTask/UI/Detail/DetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DetailViewController.swift
+//  FinTask
+//
+//  Created by Tomasz Pieczykolan on 10/06/2018.
+//  Copyright © 2018 Tomasz Pieczykolan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    
+    // MARK: - Subviews
+    
+    private lazy var toolbar: UIToolbar = {
+        let toolbar = UIToolbar()
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        toolbar.delegate = self
+        toolbar.barStyle = .black
+        let closeItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(closeButtonPressed))
+        toolbar.items = [closeItem]
+        return toolbar
+    }()
+    
+    
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViewHierarchy()
+        setupConstraints()
+        
+        view.backgroundColor = .defaultBackground
+    }
+    
+    
+    
+    // MARK: - Setup
+    
+    private func setupViewHierarchy() {
+        view.addSubview(toolbar)
+    }
+    
+    private func setupConstraints() {
+        view.addConstraints([
+            topLayoutGuide.bottomAnchor.constraint(equalTo: toolbar.topAnchor),
+            NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: toolbar, attribute: .leading, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: toolbar, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        ])
+    }
+    
+    
+    
+    // MARK: - Actions
+    
+    @objc func closeButtonPressed() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DetailViewController: UIToolbarDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
